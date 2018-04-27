aa7173f8c456e3e3624810d24afa26595fe12c90
Ionut Mihalache <ionut.mihalache@elefant.ro>
2018-04-27 19:28:32 +0300

    created webdriver factory and wired with spring boot

---
+++ src/main/java/com/frontend/fe/webdriver/WebDriverFactory.java
@@ -0,0 +1,20 @@
+package com.frontend.fe.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebDriverFactory {
+
+	public WebDriver getInstance() {
+		setFirefoxDriver();
+		return new FirefoxDriver();
+	}
+
+	private static void setFirefoxDriver() {
+		String os = System.getProperty("os.name").toLowerCase().substring(0, 3);
+		String firefoxBinary = "src/main/resources/geckodriver" + (os.equals("win") ? ".exe" : "");
+		System.setProperty("webdriver.gecko.driver", firefoxBinary);
+	}
+}
--- src/test/java/com/frontend/fe/TestBase.java
+++ src/test/java/com/frontend/fe/TestBase.java
@@ -1,22 +1,28 @@
 package com.frontend.fe;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
+import com.frontend.fe.webdriver.WebDriverFactory;
+
 @ContextConfiguration(locations = { "classpath:spring-test-config.xml" })
-public class TestBase extends AbstractTestNGSpringContextTests{
+public class TestBase extends AbstractTestNGSpringContextTests {
+
+	@Autowired
+	WebDriverFactory factory;
+
+	WebDriver driver;
 
 	@Value("${site.url}")
 	private String siteUrl;
 
 	@Test
 	public void init() {
-		System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-		WebDriver driver = new FirefoxDriver();
+		driver = factory.getInstance();
 		driver.get(siteUrl);
 	}
 }
